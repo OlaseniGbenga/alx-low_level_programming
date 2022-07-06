87e9f7a647419a9d2bf6d7afcbc7049757c2d8af
Olaseni Gbenga <gj.olaseni@gmail.com>
2022-07-06 01:14:27 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,33 +1,27 @@
 #include "main.h"
 
 /**
+ * print_alphabet - print out alphabets
  *
- *  * print_alphabet - prints out alphabets
- *
- *   *
- *
- *    * Return: always 0
- *
- *     */
+ * Return: Always 0.
+ */
 
 void print_alphabet(void)
 
 {
+char alpha = 97;
 
-		char alpha = 97;
+while (alpha < 123)
 
+{
 
+_putchar(alpha);
 
-			while (alpha < 123)
-
-					{
-
-								_putchar(alpha);
-
-										alpha++;
-
-											}
-
-				_putchar('\n');
+alpha++;
 
 }
+
+_putchar('\n');
+
+}
+
