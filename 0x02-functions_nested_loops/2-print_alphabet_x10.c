2dd3c4eb90184e0c79db4352064666e3f319b685
Olaseni Gbenga <gj.olaseni@gmail.com>
2022-07-06 01:45:23 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,35 +1,34 @@
 #include "main.h"
 
 /**
- *
- *  * print_alphabet_x10 - prints lowercase alpabets 10 times, each on a new line.
- *
- *   */
+ * print_alphabet_x10 - prints lowercase alpabets 10 times, each on a new line.
+ */
 
 void print_alphabet_x10(void)
 
 {
 
-		int num;
+int num;
 
-			char alpha;
+char alpha;
 
 
 
-				for (num = 1; num <= 10; num++)
+for (num = 1; num <= 10; num++)
 
-						{
+{
 
-									for (alpha = 97; alpha < 123; alpha++)
+for (alpha = 97; alpha < 123; alpha++)
 
-												{
+{
 
-																_putchar(alpha);
-
-																		}
-
-											_putchar('\n');
-
-												}
+_putchar(alpha);
 
 }
+
+_putchar('\n');
+
+}
+
+}
+
